12d6f755b44c901897cb126234f37b463a119d63
Raphael <raphaelvictor27@gmail.com>
2024-04-26 07:32:14 -0300

    1st commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("GET /ping", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("pong"))
+	})
+	http.ListenAndServe(":8080", nil)
+}
